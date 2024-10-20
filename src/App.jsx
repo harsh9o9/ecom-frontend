42c8aae6f7dac417bd67d87bf20d7fc2e64143e7
harsh9o9 <hbhardwaj454@gmail.com>
2024-10-20 19:54:52 +0530

    Refactor category route changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Product from './pages/Product';
-import Category from './pages/Categories';
+import CategoriesHome from './pages/CategoriesHome';
+import CategoryPage from './pages/CategoryPage';
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/product/:id" element={<Product />} />
-
-            <Route path="/categories" element={Category} />
+            <Route path="/categories" element={<CategoriesHome />} />
+            <Route path="/category/:id" element={<CategoryPage />} />
         </Routes>
     );
 }
--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-
-const CategoryCard = (props) => {
-
-    return(<div>
-        <Link 
-        className="block max-w-sm rounded-lg border border-gray-700 bg-gray-800 p-6 shadow hover:bg-gray-700"
-        to={`/category/${props?.id || ''}`}
-        <p className="mb-2 text-2xl font-bold tracking-tight text-white">
-            {props.name}
-        </p>
-        />
-    </div>);
+const CategoryCard = ({ categoryData }) => {
+    return (
+        <div>
+            <Link
+                className="block max-w-sm rounded-lg border border-gray-700 bg-gray-800 p-6 shadow hover:bg-gray-700"
+                to={`/category/${categoryData?.id || ''}`}>
+                <p className="mb-2 text-2xl font-bold tracking-tight text-white">
+                    {categoryData?.name}
+                </p>
+            </Link>
+        </div>
+    );
 };
 
 export default CategoryCard;
